9db4bf9ac01a44890ff1e50e0a3355f2e1b946c4
gonchaba <ggonchaba@gmail.com>
2023-12-13 16:35:52 +0300

    Okta configurations

--- src/main/java/com/gonchaba/maps/MapsApplication.java
+++ src/main/java/com/gonchaba/maps/MapsApplication.java
@@ -1,14 +1,19 @@
 package com.gonchaba.maps;
 
 import com.gonchaba.maps.config.GoogleMapsConfig;
-import com.netflix.discovery.EurekaNamespace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 
 @SpringBootApplication(scanBasePackages = "com.gonchaba.maps", exclude = DataSourceAutoConfiguration.class)
 @EnableFeignClients
@@ -17,5 +22,20 @@ import org.springframework.context.annotation.ComponentScan;
 public class MapsApplication {
     public static void main(String[] args) {
         SpringApplication.run(MapsApplication.class, args);
+    }
+
+    @Bean
+    public OAuth2AuthorizedClientManager clientManager(ClientRegistrationRepository clientRegistrationRepository,
+                                                       OAuth2AuthorizedClientRepository auth2AuthorizedClientRepository) {
+        OAuth2AuthorizedClientProvider auth2AuthorizedClientProvider
+                = OAuth2AuthorizedClientProviderBuilder.builder()
+                .clientCredentials()
+                .build();
+        DefaultOAuth2AuthorizedClientManager defaultOAuth2AuthorizedClientManager
+                = new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository,
+                auth2AuthorizedClientRepository);
+        defaultOAuth2AuthorizedClientManager.setAuthorizedClientProvider(auth2AuthorizedClientProvider);
+
+        return clientManager(clientRegistrationRepository, auth2AuthorizedClientRepository);
     }
 }
---
+++ src/main/java/com/gonchaba/maps/security/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.gonchaba.maps.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig {
+
+    @Bean
+    public DefaultSecurityFilterChain securityWebFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .authorizeRequests(
+                        authorizeRequest -> {
+                            try {
+                                authorizeRequest
+                                        .anyRequest()
+                                        .authenticated()
+                                        .and().oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                );
+       return httpSecurity.build();
+    }
+}
